2b55941000806d7346c92caee19b4f5b62595a6f
Aleksey Shirokih <shirokih@nocproject.org>
2019-06-02 01:32:29 +0300

    Remove clickhouse

--- teardown.sh
+++ teardown.sh
@@ -5,5 +5,7 @@ kubectl delete -f noc/
 kubectl delete -f system/nsq
 kubectl delete pvc datadir-nsq-nsqd-0
 
+kubectl delete -f system/clickhouse
+
 helm delete --purge mongo 
 helm delete --purge postgres
