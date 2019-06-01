b0f5e95186b4e4e6b91d1d59a3ba1c9cbc6bd20c
Aleksey Shirokih <shirokih@nocproject.org>
2019-06-02 01:32:11 +0300

    Install clickhouse

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set -xe
 
 # install and init tiller
 kubectl apply -f system/tiller.yaml
-helm init --service-account tiller --history-max 1
+helm init --service-account tiller --history-max 1 --upgrade
 
 #init noc configmap and secrets
 kubectl apply -f noc/noc-configmap.yaml
@@ -15,6 +15,9 @@ kubectl apply -f noc/noc-secret.yaml
 
 # run nsqd
 kubectl apply -f system/nsq/
+
+# run clickhouse
+kubectl apply -f system/clickhouse/
 
 #init dbs
 helm upgrade --install postgres stable/postgresql --values system/postgres/values.yaml
@@ -24,6 +27,5 @@ helm upgrade --install mongo stable/mongodb --values system/mongo/values.yaml
 kubectl apply -f system/tls-secret.yaml
 
 
-#init noc
-kubectl apply -f noc/init-job.yaml 
-
+# init noc
+kubectl apply -f noc/init-job.yaml
