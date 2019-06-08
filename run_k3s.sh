93fbc8d29c9ae56e02af9de1c5c38de80bbed65b
Aleksey Shirokih <shirokih@nocproject.org>
2019-06-08 16:19:22 +0300

    Fix k3s run path

--- run_k3s.sh
+++ run_k3s.sh
@@ -2,8 +2,8 @@
 
 # install local-path provisioner
 
-kubectl apply -f env/k3s/volumes
+kubectl apply -f overlays/k3s/volumes
 
 kubectl get storageclass
 
-kubectl apply -f env/k3s/metrics_server_k3s/
+kubectl apply -f overlays/k3s/metrics_server_k3s/
